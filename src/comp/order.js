a5fbf1c572e4f949a3f5c60745d6821630d7a624
vikash <vikashraj144@gmail.com>
2019-11-14 01:43:38 +0530

    order comp added

---
+++ src/comp/order.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import * as Constants from '../../utils/Constants';
+
+class Order extends React.Component { 
+      render() {
+          return(
+            <div>
+                <p>testing</p>
+            </div>
+          )
+      }
+
+}
+
+export default Order;
