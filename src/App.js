b3ca2d266acfebb7350d38412519d9c013892ccf
Verma <Vikash.Verma@pepsico.com>
2020-01-20 12:49:30 +0530

    all setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Order from './comp/order';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Order></Order>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
