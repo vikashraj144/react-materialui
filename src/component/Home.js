c21216efa3d6e623d2f288ff10b4d7d17b9f63e8
Verma <Vikash.Verma@pepsico.com>
2020-01-28 11:52:22 +0530

    home component added

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
+// import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { TextField, IconButton, InputAdornment, FormControl, InputLabel, Input } from '@material-ui/core';
+import { IconButton, InputAdornment, FormControl, InputLabel, Input } from '@material-ui/core';
 import clsx from 'clsx';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
